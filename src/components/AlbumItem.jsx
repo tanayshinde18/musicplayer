5bd54d40c2048b69d3c59a0d33fa82fe95a4ea9d
tanayshinde18 <tanayshinde1820@gmail.com>
2024-09-26 21:33:50 +0530

    changes made to solve errors in AlbumItem and SongItem

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -6,7 +6,7 @@ const AlbumItem = ({image,name,desc,id}) => {
   return (
     <div onClick={()=>navigation(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className='rounded 'src={image} alt="" />
-        <p classname='font-bold mt-2 mb-1'>{name}</p>
+        <p className='font-bold mt-2 mb-1'>{name}</p>
         <p className='text-slate-200 text-sm'>{desc}</p> 
       
     </div>
--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -9,7 +9,7 @@ const SongItem = ({name,image,desc,id}) => {
   return (
     <div onClick={()=> playWithId(id)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff25]'>
       <img className='rounded' src={image} alt="" />
-      <p classname='font-bold mt-2 mb-1'>{name}</p>
+      <p className='font-bold mt-2 mb-1'>{name}</p>
       <p className='text-slate-200 text-sm'>{desc}</p>
     </div>
   )
